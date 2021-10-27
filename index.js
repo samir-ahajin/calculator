8e255daf9a7c660930939b27f4c378f699d755d5
Samir Ahajin <samirahajin@gmail.com>
2021-10-27 15:30:12 +0800

    added another div which show the equation after result and functionality of C and CE(not yet final)

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let total = document.querySelector('.result');
+let usedequation = document.querySelector('.equation')
+let symbol= ["x","/","+","-"]
+
 function getSum(firstNum,secondNum){
     return parseFloat(firstNum)+ parseFloat(secondNum);
 }
@@ -14,35 +17,31 @@ function getQuo(firstNum,secondNum){
 function getTotal(){
     let all = Array.from(total.querySelectorAll("p"));
     let equation = all.map(each=>each.textContent)
-    const symbol= ["x","/","+","-"]
+    usedequation.textContent = equation.join(" ") +" =";
     for(let counter = 0; counter < symbol.length;counter++){
         let mdas = equation.findIndex(a => a === symbol[counter]); 
-        if(!(mdas === -1)){
+        if(mdas !== -1){
             let sign = equation.filter(sign=>sign === symbol[counter]).length; 
             while(sign != 0){  
                 switch(symbol[counter]){
                     case "x":
-                        let prod = getProd(equation[mdas-1],equation[mdas+1]);
-                        equation.splice(mdas-1,3);
-                        equation.unshift(prod); 
+                        let prod = String(getProd(equation[mdas-1],equation[mdas+1]));
+                        equation.splice(mdas-1,3,prod);
                         console.log(prod);
                         break;
                     case "/":
-                        let quo = getQuo(equation[mdas-1],equation[mdas+1]);
-                        equation.splice(mdas-1,3);
-                        equation.unshift(quo);
+                        let quo = String(getQuo(equation[mdas-1],equation[mdas+1]));
+                        equation.splice(mdas-1,3,quo);
                         console.log(quo);
                         break;
                     case "+":
-                        let sum = getSum(equation[mdas-1],equation[mdas+1]);
-                        equation.splice(mdas-1,3);
-                        equation.unshift(sum);
+                        let sum = String(getSum(equation[mdas-1],equation[mdas+1]));
+                        equation.splice(mdas-1,3,sum);
                         console.log(sum);
                         break;
                     case "-":
-                        let diff = getDiff(equation[mdas-1],equation[mdas+1]);
-                        equation.splice(mdas-1,3);
-                        equation.unshift(diff);
+                        let diff = String(getDiff(equation[mdas-1],equation[mdas+1]));
+                        equation.splice(mdas-1,3,diff);
                         console.log(diff);
                         break;
                     default:
@@ -55,26 +54,25 @@ function getTotal(){
             continue;
         }    
     }
-    console.log(equation+"final");   
+    total.textContent = equation;  
 }
 function insertNewValue(val){
     let newValue = document.createElement("p");
 
-    if(val == "+" || val== "-" || val== "/" || val== "x"){
+    if(symbol.includes(val)){
         console.log(val+"a");
         newValue.innerHTML = val;
         total.appendChild(newValue);
     }else{
         let lastNode = total.lastElementChild
         let newNumber = document.createTextNode(`${val}`);
-        if(!(total.hasChildNodes()) ||  lastNode.textContent == "+" || lastNode.textContent == "-" || lastNode.textContent== "/" || lastNode.textContent== "x" ){
+        if(!(total.hasChildNodes()) ||  symbol.includes(lastNode.textContent)){
             console.log(val+"s");
             newValue.innerHTML = val;
             total.appendChild(newValue);
         }
         else{
-            console.log(total.lastElementChild.textContent);
-           total.lastElementChild.appendChild(newNumber);
+          lastNode.appendChild(newNumber);
           
         }
         
@@ -83,12 +81,26 @@ function insertNewValue(val){
 }
 let numpad = document.querySelectorAll(".button");
 numpad.forEach(npad =>npad.addEventListener('click',function(e){
-    if(e.target.textContent === "="){
-        getTotal();
-    }else{
-        if(total.textContent === "0"){
-            total.textContent ="";
-        }
-        insertNewValue(e.target.textContent);
+    console.log(e.target.textContent);
+    switch(e.target.textContent){
+        case "=":
+            getTotal();
+            break;
+        case "C":
+            total.textContent ="0";
+            break;
+        case "CE":
+            let lastNode = total.lastElementChild
+            if(total.hasChildNodes() && symbol.includes(lastNode.textContent)){
+                total.removeChild(lastNode);
+            }
+            break;
+        default:
+            if(total.textContent === "0"){
+                total.textContent ="";
+            }
+            insertNewValue(e.target.textContent);
+            break;
     }
+   
 }))
